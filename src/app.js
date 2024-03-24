04f4143c144f37177d2cb798b8cb06bbccba5991
rosemulazada <rosemulazada16@gmail.com>
2024-03-24 02:59:03 +0100

    ADD: MIT License

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
-// core modules
+// Core modules
 const path = require("path");
 
-// npm modules
+// NPM modules
 const express = require("express");
-const ejs = require("ejs");
 
 const app = express();
-const port = 8000;
+const PORT = process.env.PORT || 8000;
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
 app.set("view engine", "ejs");
@@ -19,12 +17,18 @@ app.set("views", viewsPath);
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
+/**========================================================================
+ *                           CODE START
+ *========================================================================**/
+
 app.get("", (req, res) => {
     res.render("index");
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}.`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
 });
 
 module.exports = app;
+
+console.log(process.env);
